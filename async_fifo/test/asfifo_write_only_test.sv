07c93bc91aafd31af65c9ced59b2059ea47dff0f
Anadi Chauhan <anadichauhan454@gmail.com>
2026-08-06 23:40:48 +0530

    Create asfifo_write_only_test.sv

---
+++ async_fifo/test/asfifo_write_only_test.sv
@@ -0,0 +1,11 @@
+class asfifo_write_only_test #(int DWIDTH = 8) extends asfifo_base_test #(DWIDTH);
+  
+  function new(virtual asfifo_if #(DWIDTH) vif);
+    super.new(vif);
+  endfunction
+  
+  virtual task configure();
+    env.gen.count = 50;
+    env.gen.mode = WRITE_ONLY;
+  endtask
+endclass
