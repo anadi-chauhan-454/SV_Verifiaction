ad0fc0d217bb40c26c6f633b06d577543d6c53b7
Anadi Chauhan <anadichauhan454@gmail.com>
2026-08-12 12:09:34 +0530

    Create riscv_pkg.sv

---
+++ riscv-5stage-pipelined-cpu/package/riscv_pkg.sv
@@ -0,0 +1,47 @@
+package riscv_rpkg;
+
+  typedef enum logic [6:0] {
+        R_TYPE = 7'b0110011,
+        I_TYPE = 7'b0010011,
+        LOAD   = 7'b0000011,
+        STORE  = 7'b0100011,
+        BRANCH = 7'b1100011
+    } opcode_sel;
+
+
+    typedef enum logic [2:0]{
+      IM_I_TYPE,
+      S_TYPE,
+      B_TYPE,
+      U_TYPE,
+      J_TYPE
+    } imm_states;
+
+
+    typedef enum logic [1:0]{
+      ADD,
+      SUB,
+      RC_TYPE,
+      IC_TYPE
+    } alu_op_sel;
+
+
+
+   typedef enum logic [3:0] {
+        ALU_ADD = 4'd0,
+        ALU_SUB = 4'd1,
+        ALU_AND = 4'd2,
+        ALU_OR  = 4'd3,
+        ALU_XOR = 4'd4,
+        ALU_SHL = 4'd5,
+        ALU_SHR = 4'd6
+    } alu_sel;
+
+   typedef struct packed {
+     logic zero;
+     logic carry;
+     logic overflow;
+     logic negative;
+   } status_flags;
+
+endpackage
