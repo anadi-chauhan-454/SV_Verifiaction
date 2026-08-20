8caae6c5941907c8413f50044a0387f59e6d15c0
Anadi Chauhan <anadichauhan454@gmail.com>
2026-08-21 01:10:22 +0530

    Update asfifo_env.sv

--- async_fifo/testbench/asfifo_env.sv
+++ async_fifo/testbench/asfifo_env.sv
@@ -1,6 +1,6 @@
 class asfifo_env #(
-  int DWIDTH=8,
-  int DEPTH =8
+  int DWIDTH=16,
+  int DEPTH =16
 );
   
   asfifo_gen #(DWIDTH) gen; 
@@ -16,10 +16,18 @@ class asfifo_env #(
   
   mailbox #(asfifo_tr #(DWIDTH)) gen2wdrv;
   mailbox #(asfifo_tr #(DWIDTH)) gen2rdrv;
-  mailbox #(asfifo_tr #(DWIDTH)) gen2rm;
+  
+  mailbox #(asfifo_tr #(DWIDTH)) wdrv2rm;
+  mailbox #(asfifo_tr #(DWIDTH)) rdrv2rm;
+  
   mailbox #(asfifo_tr #(DWIDTH)) wmon2scb;
   mailbox #(asfifo_tr #(DWIDTH)) rmon2scb;
-  mailbox #(asfifo_tr #(DWIDTH)) rm2scb;
+  
+  mailbox #(asfifo_tr #(DWIDTH)) rmw2scb;
+  mailbox #(asfifo_tr #(DWIDTH)) rmr2scb;
+  
+  mailbox #(int unsigned) w_id_mbx;
+  mailbox #(int unsigned) r_id_mbx;
   
   virtual asfifo_if #(DWIDTH) vif;
   
@@ -29,19 +37,31 @@ class asfifo_env #(
   
   task build();
     gen2wdrv = new();
-    gen2rdrv = new();;
-    gen2rm = new();;
+    gen2rdrv = new();
+    
+    wdrv2rm  = new();
+    rdrv2rm  = new();
+    
     wmon2scb = new();
     rmon2scb = new();
-    rm2scb = new();
     
-    gen = new(gen2wdrv, gen2rdrv, gen2rm);
-    wdrv = new(gen2wdrv, vif);
-    rdrv = new(gen2rdrv, vif);
-    wmon = new(wmon2scb, vif);
-    rmon = new(rmon2scb, vif);
-    rm = new(gen2rm, rm2scb, vif);
-    scb = new(wmon2scb, rmon2scb, rm2scb);
+    rmw2scb  = new();
+    rmr2scb  = new();
+    
+    w_id_mbx = new();
+    r_id_mbx = new();
+    
+    gen = new(gen2wdrv, gen2rdrv);
+    
+    wdrv = new(gen2wdrv, wdrv2rm, w_id_mbx, vif);
+    rdrv = new(gen2rdrv, rdrv2rm, r_id_mbx, vif);
+    
+    wmon = new(wmon2scb, w_id_mbx, vif);
+    rmon = new(rmon2scb, r_id_mbx, vif);
+    
+    rm = new(wdrv2rm, rdrv2rm, rmr2scb, rmw2scb, vif);
+    
+    scb = new(wmon2scb, rmon2scb, rmw2scb, rmr2scb);
   endtask
   
   task run();
@@ -55,7 +75,7 @@ class asfifo_env #(
       scb.run();
     join_none
     @(gen.done);
-    #500;
+    #5000;
     scb.report();
     $finish;
   endtask
