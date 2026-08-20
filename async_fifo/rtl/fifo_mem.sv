3da49f56d6ed5c86ee95548bdb2ac5f6acdc280d
Anadi Chauhan <anadichauhan454@gmail.com>
2026-08-21 01:05:41 +0530

    Update fifo_mem.sv

--- async_fifo/rtl/fifo_mem.sv
+++ async_fifo/rtl/fifo_mem.sv
@@ -1,34 +1,40 @@
 module fifo_mem #(
-  int DWIDTH = 8,
-  int DEPTH  = 8,
-  int PWIDTH = 4
+  int DWIDTH = 16,
+  int DEPTH  = 16,
+  int PWIDTH = $clog2(DEPTH) + 1
 )
-  (
-    input logic wclk,
-    input logic wen,
-    input logic [PWIDTH-1:0] bwptr,
-    input logic full,
-    input logic rclk, rrst_n,
-    input logic ren,
-    input logic [PWIDTH-1:0] brptr,
-    input logic empty,
-    
-    input logic  [DWIDTH-1:0] data_in, 
-    output logic [DWIDTH-1:0] data_out
-  );
-  
+(
+  input logic wclk,
+  input logic wen,
+  input logic [PWIDTH-1:0] bwptr,
+  input logic full,
+
+  input logic rclk,
+  input logic rrst_n,
+  input logic ren,
+  input logic [PWIDTH-1:0] brptr,
+  input logic empty,
+
+  input  logic [DWIDTH-1:0] data_in,
+  output logic [DWIDTH-1:0] data_out
+);
+
+  localparam int ADDR_WIDTH = $clog2(DEPTH);
   logic [DWIDTH-1:0] fifo_mem [0:DEPTH-1];
-  
-  always @(posedge wclk) begin
-    if(wen && !full)
-      fifo_mem[bwptr[PWIDTH-1:0]] <= data_in;
+
+  always_ff @(posedge wclk) begin
+    if (wen && !full)
+      fifo_mem[bwptr[ADDR_WIDTH-1:0]] <= data_in;
   end
-  
+
   always_ff @(posedge rclk or negedge rrst_n) begin
-    if(!rrst_n)
+    if (!rrst_n) begin
       data_out <= '0;
-    else if(ren && !empty)
-      data_out <= fifo_mem[brptr[PWIDTH-1:0]];
+    end
+    else if (ren && !empty) begin
+      data_out <= fifo_mem[brptr[ADDR_WIDTH-1:0]];
+    end
+
   end
+
 endmodule
-      
