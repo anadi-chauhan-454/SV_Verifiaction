9e1311e98bbbe0256697084a2276683d66be323f
Anadi Chauhan <anadichauhan454@gmail.com>
2026-08-21 01:09:44 +0530

    Update asfifo_rm.sv

--- async_fifo/testbench/asfifo_rm.sv
+++ async_fifo/testbench/asfifo_rm.sv
@@ -1,49 +1,87 @@
 class asfifo_rm #(
-  int DWIDTH=8,
-  int DEPTH=8
+  int DWIDTH = 16,
+  int DEPTH  = 16
 );
-  
-  mailbox #(asfifo_tr #(DWIDTH)) in_mbx;
-  mailbox #(asfifo_tr #(DWIDTH)) out_mbx;
+
+  mailbox #(asfifo_tr #(DWIDTH)) win_mbx;
+  mailbox #(asfifo_tr #(DWIDTH)) rin_mbx;
+
+  mailbox #(asfifo_tr #(DWIDTH)) outr_mbx;
+  mailbox #(asfifo_tr #(DWIDTH)) outw_mbx;
+
   virtual asfifo_if #(DWIDTH) vif;
-  
-  
+
   bit [DWIDTH-1:0] queue[$];
-  
-  function new(mailbox #(asfifo_tr #(DWIDTH)) in_mbx,
-               mailbox #(asfifo_tr #(DWIDTH)) out_mbx,
-               virtual asfifo_if #(DWIDTH) vif);
-    this.in_mbx = in_mbx;
-    this.out_mbx = out_mbx;
-    this.vif = vif;
+
+  function new(
+    mailbox #(asfifo_tr #(DWIDTH)) win_mbx,
+    mailbox #(asfifo_tr #(DWIDTH)) rin_mbx,
+    mailbox #(asfifo_tr #(DWIDTH)) outr_mbx,
+    mailbox #(asfifo_tr #(DWIDTH)) outw_mbx,
+    virtual asfifo_if #(DWIDTH) vif
+  );
+
+    this.win_mbx  = win_mbx;
+    this.rin_mbx  = rin_mbx;
+    this.outr_mbx = outr_mbx;
+    this.outw_mbx = outw_mbx;
+    this.vif      = vif;
+
   endfunction
-  
+
   task run();
+    fork
+      write_domain();
+      read_domain();
+    join_none
+  endtask
+  
+  task write_domain();
     asfifo_tr #(DWIDTH) tr;
     asfifo_tr #(DWIDTH) exp_tr;
-    
     forever begin
-      
-      in_mbx.get(tr);
+      @(vif.wdrv_cb);
+      win_mbx.get(tr);
+      if (!vif.wrst_n) begin
+        queue.delete();
+        continue;
+      end
       exp_tr = tr.copy();
-      
-      if(!vif.wrst_n || !vif.rrst_n) queue.delete();
-      
-      if(tr.wen && !tr.full)
-        queue.push_back(tr.data_in);
-      if(tr.ren && !tr.empty)
-        exp_tr.data_out = queue.pop_front();
-      
-      exp_tr.full = (queue.size() == DEPTH);
-      exp_tr.empty = (queue.size() == 0);
-      
-      exp_tr.display("RM");
-      out_mbx.put(exp_tr);
+      if (tr.wen && !vif.wmon_cb.full) begin
+        if (queue.size() < DEPTH) begin
+          queue.push_back(tr.data_in);
+          outw_mbx.put(exp_tr);
+          exp_tr.display("WRM");
+        end
+        else begin
+          $error("[RM] Reference FIFO overflow");
+        end
+      end
+    end
+  endtask
+
+  task read_domain();
+    asfifo_tr #(DWIDTH) tr;
+    asfifo_tr #(DWIDTH) exp_tr;
+    forever begin
+      @(vif.rdrv_cb);
+      rin_mbx.get(tr);
+      if (!vif.rrst_n) begin
+        queue.delete();
+        continue;
+      end
+      exp_tr = tr.copy();
+      if (tr.ren && !tr.empty) begin
+        if (queue.size() != 0) begin
+          @(vif.rdrv_cb);
+          exp_tr.data_out = queue.pop_front();
+          outr_mbx.put(exp_tr);
+          exp_tr.display("RRM");
+        end
+        else begin
+          $error("[RM] Reference FIFO underflow");
+        end
+      end
     end
   endtask
 endclass
-      
-      
-    
-  
-  
