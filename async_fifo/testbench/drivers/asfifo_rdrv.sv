7c61257d782388dcfdb575a733eac30a44b38b8c
Anadi Chauhan <anadichauhan454@gmail.com>
2026-08-21 01:08:20 +0530

    Update asfifo_rdrv.sv

--- async_fifo/testbench/drivers/asfifo_rdrv.sv
+++ async_fifo/testbench/drivers/asfifo_rdrv.sv
@@ -1,31 +1,44 @@
-class asfifo_rdrv #(DWIDTH = 8);
+class asfifo_rdrv #(int DWIDTH = 16);
   
   mailbox #(asfifo_tr #(DWIDTH)) mbx;
+  mailbox #(asfifo_tr #(DWIDTH)) rrm_mbx;
+  mailbox #(int unsigned) r_id_mbx;
   
   virtual asfifo_if #(DWIDTH) vif;
   
   function new(mailbox #(asfifo_tr #(DWIDTH)) mbx,
+               mailbox #(asfifo_tr #(DWIDTH)) rrm_mbx,
+               mailbox #(int unsigned) r_id_mbx,
   			   virtual asfifo_if #(DWIDTH) vif);
     this.mbx = mbx;
+    this.rrm_mbx = rrm_mbx;
+    this.r_id_mbx = r_id_mbx;
     this.vif = vif;
   endfunction
   
   task reset();
-    wait(!vif.rrst_n)
+    wait(!vif.rrst_n);
     
     vif.rdrv_cb.ren <= 0;
     
     wait(vif.rrst_n);
+    @(vif.rdrv_cb);
   endtask
   
-  task drive(asfifo_tr #(DWIDTH) tr);
+task drive(asfifo_tr #(DWIDTH) tr);
+
     @(vif.rdrv_cb);
-    
+    tr.empty = vif.rdrv_cb.empty;
+
+    if (tr.empty)
+        tr.ren = 0;
     vif.rdrv_cb.ren <= tr.ren;
-    
+
     @(vif.rdrv_cb);
+
     vif.rdrv_cb.ren <= 0;
-  endtask
+
+endtask
   
   task run();
     asfifo_tr #(DWIDTH) tr;
@@ -35,6 +48,11 @@ class asfifo_rdrv #(DWIDTH = 8);
       mbx.get(tr);
       tr.display("RDRV");
       drive(tr);
+      tr.display("AFTER RDRV");
+      if (tr.ren) begin
+        rrm_mbx.put(tr.copy());
+        r_id_mbx.put(tr.txn_id);
+      end
     end
   endtask
 endclass
