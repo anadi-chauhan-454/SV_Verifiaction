d1ec52651acfbaece67502d73c84213c5b906a38
Anadi Chauhan <anadichauhan454@gmail.com>
2026-08-21 01:08:04 +0530

    Update asfifo_wdrv.sv

--- async_fifo/testbench/drivers/asfifo_wdrv.sv
+++ async_fifo/testbench/drivers/asfifo_wdrv.sv
@@ -1,12 +1,18 @@
-class asfifo_wdrv #(int DWIDTH=8);
+class asfifo_wdrv #(int DWIDTH=16);
   
   mailbox #(asfifo_tr #(DWIDTH)) mbx;
+  mailbox #(asfifo_tr #(DWIDTH)) wrm_mbx;
+   mailbox #(int unsigned) w_id_mbx;
   
   virtual asfifo_if #(DWIDTH) vif;
   
   function new(mailbox #(asfifo_tr #(DWIDTH)) mbx,
+               mailbox #(asfifo_tr #(DWIDTH)) wrm_mbx,
+               mailbox #(int unsigned) w_id_mbx,
   			   virtual asfifo_if #(DWIDTH) vif);
     this.mbx = mbx;
+    this.wrm_mbx = wrm_mbx;
+    this.w_id_mbx = w_id_mbx;
     this.vif = vif;
   endfunction
   
@@ -22,6 +28,11 @@ class asfifo_wdrv #(int DWIDTH=8);
   
   task drive(asfifo_tr #(DWIDTH) tr);
     @(vif.wdrv_cb);
+    
+    tr.full = vif.wdrv_cb.full;
+
+    if (tr.full)
+        tr.wen = 0;
     
     vif.wdrv_cb.wen <= tr.wen;
     
@@ -40,6 +51,11 @@ class asfifo_wdrv #(int DWIDTH=8);
       mbx.get(tr);
       tr.display("WDRV");
       drive(tr);
+      @(vif.wdrv_cb);
+      if (tr.wen) begin
+        wrm_mbx.put(tr.copy());
+        w_id_mbx.put(tr.txn_id);
+      end
     end
   endtask
 endclass
