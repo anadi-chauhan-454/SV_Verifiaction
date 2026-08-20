48934d0bced5b11dd4499cc510383c7854c7674c
Anadi Chauhan <anadichauhan454@gmail.com>
2026-08-21 01:09:22 +0530

    Update asfifo_rmon.sv

--- async_fifo/testbench/monitors/asfifo_rmon.sv
+++ async_fifo/testbench/monitors/asfifo_rmon.sv
@@ -1,12 +1,15 @@
-class asfifo_rmon #(int DWIDTH=8);
+class asfifo_rmon #(int DWIDTH=16);
   mailbox #(asfifo_tr #(DWIDTH)) mbx;
+  mailbox #(int unsigned) id_mbx;
   virtual asfifo_if #(DWIDTH) vif;
   
   asfifo_cov #(DWIDTH) cov;
   
   function new(mailbox #(asfifo_tr #(DWIDTH)) mbx,
+               mailbox #(int unsigned) id_mbx,
   			   virtual asfifo_if #(DWIDTH) vif);
     this.mbx = mbx;
+    this.id_mbx = id_mbx;
     this.vif = vif;
     
     cov = new();
@@ -14,18 +17,19 @@ class asfifo_rmon #(int DWIDTH=8);
   
   task run();
     asfifo_tr #(DWIDTH) tr;
+    int unsigned txn_id;
     
-    forever begin
-      @(vif.rmon_cb);
-      tr= new();
-      
-      tr.ren =  vif.rmon_cb.ren;
-      tr.data_out = vif.rmon_cb.data_out;
-      tr.empty = vif.rmon_cb.empty;
-      
-      tr.display("RMON");
-      mbx.put(tr.copy());
-      cov.write(tr);
+   forever begin
+     @(vif.rmon_cb);
+       id_mbx.get(txn_id);
+       
+        tr = new();
+        tr.txn_id  = txn_id;
+        tr.data_out = vif.rmon_cb.data_out;
+        
+        tr.display("RMON");
+        mbx.put(tr.copy());
+        cov.write(tr);
     end
   endtask
 endclass
